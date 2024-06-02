9dfda6139161d1e88a457077082e65b55abb3d79
KeotiseuF <curtis.fanchonna@gmail.com>
2024-06-03 01:33:33 +0200

    Fix error display Home

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   const url = window.location.href;
-  const checkUrl = url === `${window.location.protocol}//${window.location.host}/NSM`;
+  const checkUrl = url === `${window.location.protocol}//${window.location.host}/NSM/`;
   return (
     <>
       <Header />
